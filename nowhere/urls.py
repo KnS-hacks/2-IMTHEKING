3fde1654497431bce4603648a81cc6819dfda427
qkrghd147 <qkrghd147@likelion.org>
2021-11-10 15:27:44 +0900

    add urls.py, settings.py

--- nowhere/urls.py
+++ nowhere/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from account import views as A
 from post import views as P
@@ -22,5 +25,6 @@ from post import views as P
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', P.main, name="home"),
-    
-]
+    path('account/', include('account.urls')),
+    path('post/', include('post.urls')),
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
