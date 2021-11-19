ee5ed85e2f69ba4e898a388c3cda2358c632490f
qkrghd147 <qkrghd147@likelion.org>
2021-11-20 03:20:18 +0900

    위도경도 기능 수정

--- post/urls.py
+++ post/urls.py
@@ -4,7 +4,7 @@ from .views import *
 urlpatterns = [
     path('upload/', upload, name="upload"),
     path('main/', main, name="main"),
-    path('selectlocation/', location, name="selectlocation"),
+    path('selectlocation/<int:Concert_id>/', location, name="selectlocation"),
     path('update/<int:Concert_id>/', update, name="update"), # 수정됨
     path('detail/<int:Concert_id>/', detail, name="detail"), #수정됨
     path('delete/<int:Concert_id>/', delete, name="delete"), #수정됨
