981d607917fa07330e29b96e46d34ef4677991a9
qkrghd147 <qkrghd147@likelion.org>
2021-11-19 20:41:55 +0900

    회원가입 기능 구현 완료

--- account/models.py
+++ account/models.py
@@ -1,8 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from .managers import UserManager
+
+# Create your models here.
 
 class User(AbstractUser):
-  nickname = models.CharField(max_length=40)
 
-  def __str__(self):
-    return self.nickname
+   username = None
+   
+   username = models.CharField(default="username", max_length=64)
+   email = models.EmailField(default="email", max_length=64, unique=True)
+   password = models.CharField(default="password", max_length=64)
+
+   objects = UserManager()
+
+   USERNAME_FIELD = 'email'
+   REQUIRED_FIELDS = []
+
+
+   def __str__(self): 
+        return self.username
--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import *
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('signup/', signup, name="signup"),
-    path('login', login, name="login"),
+    path('login/', views.login, name="login"),
+    path('signin/', views.signin, name="signin"),
+    path('signup/', views.signup, name="signup"),
+    path('logout/', views.logout, name="logout"),
+    path('signin/fi/', views.fi, name='fi'),
 ]
