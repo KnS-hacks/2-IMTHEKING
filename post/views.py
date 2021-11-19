c6f0be49f851cb1cb02d68ee73d7796fcba4d4a0
qkrghd147 <qkrghd147@likelion.org>
2021-11-20 03:17:13 +0900

    위도 경도 업데이트 씹가능

--- post/views.py
+++ post/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from .models import Concert
 import json
 
-
 # Create your views here.
 def main (request):
   concerts = Concert.objects.all()
@@ -16,29 +15,37 @@ def main (request):
       "address": concert.concert_address,
       "latitude": str(concert.latitude),
       "longitude": str(concert.longitude),
-      "id": concert.id,
     }
     concertdict.append(content)
 
   concertJson = json.dumps(concertdict)
   return render(request, 'main.html', {'concertJson':concertJson})
 
-def location(request):
-    return render(request, 'selectlocation.html') # location => select
+def location(request, Concert_id):
+    concert=Concert.objects.get(id=Concert_id)
+    if request.method == "POST":
+      concert.latitude = request.POST.get('latitude', None)
+      concert.longitude = request.POST.get('longitude', None)
+      concert.concert_address = request.POST.get('concert_address', None)
+      concert.save()
+      return redirect('/post/detail/' + str(concert.id)) # location => select
+    else:
+      return render(request, 'selectlocation.html') # location => select
 
 def upload(request):
   if request.method == "GET":
       return render(request, 'upload.html')
   elif request.method == "POST":
-      concert_title = request.POST.get('concert_title', None)
-      concert_player = request.POST.get('concert_player', None)
-      concert_location = request.POST.get('concert_location', None)
-      concert_detail = request.POST.get('concert_detail', None)
-      concert_datetime = request.POST.get('concert_datetime', None)
-      concert_image = request.FILES.get('concert_image', None)
-      upload = Concert(concert_title=concert_title, concert_player=concert_player, concert_location=concert_location, concert_detail=concert_detail, concert_image=concert_image, concert_datetime=concert_datetime)
-      upload.save()
-      return render(request, 'selectlocation.html') # location => select
+      concert = Concert()
+      concert.concert_title = request.POST.get('concert_title', None)
+      concert.concert_player = request.POST.get('concert_player', None)
+      concert.concert_location = request.POST.get('concert_location', None)
+      concert.concert_detail = request.POST.get('concert_detail', None)
+      concert.concert_datetime = request.POST.get('concert_datetime', None)
+      concert.concert_image = request.FILES.get('concert_image', None)
+      concert.save()
+      return redirect('/post/selectlocation/' + str(concert.id)) # location => select
+
 # 게시글 불러오기
 def detail(request, Concert_id):
   concert = Concert.objects.get(id=Concert_id)
@@ -65,7 +72,7 @@ def update(request, Concert_id):
       concert.concert_datetime = request.POST.get('concert_datetime', None)
       concert.concert_image = request.FILES.get('concert_image', None)
       concert.save()
-      return redirect('/post/detail/' + str(concert.id))
+      return redirect('/post/selectlocation/' + str(concert.id))
 
 def search(request):
   search_word = request.GET['search']
